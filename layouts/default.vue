7dcced7ec27d04c681d9fa9342fb83aafd5fd86b
RachidT <rachidtlemsani@gmail.com>
2025-06-09 22:07:33 +0200

    feat: add default layout and basic home page structure

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-screen bg-neutral-100">
+    <slot />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-4">
+    <header class="mb-4">
+      <h1 class="font-semibold text-lg">Edumapper*</h1>
+    </header>
+    <!-- Ici viendront les composants du formulaire -->
+    <div>
+      <!-- À compléter à l'étape suivante -->
+    </div>
+  </div>
+</template>
